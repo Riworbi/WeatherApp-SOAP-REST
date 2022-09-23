d00e0c7e4be5a946259524196e068a177a525e61
Krystian Maniewski <krystian.maniewski@atos.net>
2022-09-23 10:50:19 +0200

    update

--- src/main/java/com/example/demo/SoapWebService/SoapWSConfig.java
+++ src/main/java/com/example/demo/SoapWebService/SoapWSConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo.producingwebservice;
+package com.example.demo.SoapWebService;
 
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
--- src/main/java/com/example/demo/SoapWebService/WeatherEndPoint.java
+++ src/main/java/com/example/demo/SoapWebService/WeatherEndPoint.java
@@ -1,4 +1,4 @@
-package com.example.demo.producingwebservice;
+package com.example.demo.SoapWebService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
